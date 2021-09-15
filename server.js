ebee515ecd2c962ba9f8231a9715d4cfb8ad9d9f
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-15 13:25:04 -0500

    confirm on postman that delete is working

--- server.js
+++ server.js
@@ -34,12 +34,6 @@ app.delete('/books', deleteBook);
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
-
-  response.send('test request received');
-
-});
-
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
 async function postBooks(req, res) {
